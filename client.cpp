9b9aea4e8f316cc07e8a23696bd2fe535688d2df
Divyansh Kumar <divyansh14kr@gmail.com>
2024-07-09 18:12:45 +0530

    ported the code from C to C++ implemented event loop for multiple request handling

--- client.cpp
+++ client.cpp
@@ -89,6 +89,53 @@ static int32_t query(int fd, const char *text){
     return 0;
 }
 
+// the `query` function was simply splited into `send_req` and `read_res`.
+static int32_t send_req(int fd, const char *text) {
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg) {
+        return -1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);  // assume little endian
+    memcpy(&wbuf[4], text, len);
+    return write_all(fd, wbuf, 4 + len);
+}
+
+static int32_t read_res(int fd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("server says: %s\n", &rbuf[4]);
+    return 0;
+}
+
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
@@ -103,27 +150,18 @@ int main() {
     if (rv) {
         die("connect");
     }
-
-    // char msg[] = "hello";
-    // write(fd, msg, strlen(msg));
-
-    // char rbuf[64] = {};
-    // ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-    // if (n < 0) {
-    //     die("read");
-    // }
-    // printf("server says: %s\n", rbuf);
-    int32_t err = query(fd,"hello1");
-    if(err){
-        goto L_DONE;
+    const char *query_list[3] = {"hello1", "hello2", "hello3"};
+    for (size_t i = 0; i < 3; ++i) {
+        int32_t err = send_req(fd, query_list[i]);
+        if (err) {
+            goto L_DONE;
+        }
     }
-    err = query(fd,"hello2");
-    if(err){
-        goto L_DONE;
-    }
-    err = query(fd,"hello3");
-    if(err){
-        goto L_DONE;
+    for (size_t i = 0; i < 3; ++i) {
+        int32_t err = read_res(fd);
+        if (err) {
+            goto L_DONE;
+        }
     }
 
 L_DONE:
