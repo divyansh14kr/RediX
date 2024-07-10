0588d39e0dad599b4905bc816cf2f8fefe67380c
Divyansh Kumar <divyansh14kr@gmail.com>
2024-07-10 22:08:41 +0530

    made updates

--- client.cpp
+++ client.cpp
@@ -1,4 +1,4 @@
-#include<assert.h>
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,16 +8,18 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
-#include <stdbool.h>
+#include <string>
+#include <vector>
 
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
 
 static void die(const char *msg) {
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
-}
-static void msg(const char *msg) {
-    fprintf(stderr, "%s\n", msg);
 }
 
 static int32_t read_full(int fd, char *buf, size_t n) {
@@ -48,57 +50,26 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
 
 const size_t k_max_msg = 4096;
 
-static int32_t query(int fd, const char *text){
-    uint32_t len = (uint32_t)strlen(text);
-    if(len>k_max_msg){
-        return -1;
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+    uint32_t len = 4;
+    for (const std::string &s : cmd) {
+        len += 4 + s.size();
     }
-    char wbuf[4+ k_max_msg];
-    memcpy(wbuf,&len,4); // assuming little endian format
-    memcpy(&wbuf[4],text,len);
-    int32_t err = write_all(fd,wbuf,4+len);
-    if(err){
-        return err;
-    }
-    // 4 bytes header
-    char rbuf[4+k_max_msg+1];
-    errno = 0;
-    err = read_full(fd,rbuf,4);
-    if(err){
-        if(errno == 0){
-            msg("EOF");
-        }else{
-            msg("read() error");
-        }
-        return err;
-    }
-    memcpy(&len,rbuf,4);
-    if(len >k_max_msg){
-        msg("msg too long");
-        return -1;
-    }
-    //reply body
-    err= read_full(fd,&rbuf[4],len);
-    if(err){
-        msg("read() error");
-        return err;
-    }
-    // do something 
-    rbuf[4+len] = '\0';
-    printf("server says: %s \n", &rbuf[4]);
-    return 0;
-}
-
-// the `query` function was simply splited into `send_req` and `read_res`.
-static int32_t send_req(int fd, const char *text) {
-    uint32_t len = (uint32_t)strlen(text);
     if (len > k_max_msg) {
         return -1;
     }
 
     char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4);  // assume little endian
-    memcpy(&wbuf[4], text, len);
+    memcpy(&wbuf[0], &len, 4);  // assume little endian
+    uint32_t n = cmd.size();
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+    for (const std::string &s : cmd) {
+        uint32_t p = (uint32_t)s.size();
+        memcpy(&wbuf[cur], &p, 4);
+        memcpy(&wbuf[cur + 4], s.data(), s.size());
+        cur += 4 + s.size();
+    }
     return write_all(fd, wbuf, 4 + len);
 }
 
@@ -130,13 +101,18 @@ static int32_t read_res(int fd) {
         return err;
     }
 
-    // do something
-    rbuf[4 + len] = '\0';
-    printf("server says: %s\n", &rbuf[4]);
+    // print the result
+    uint32_t rescode = 0;
+    if (len < 4) {
+        msg("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
     return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
         die("socket()");
@@ -150,18 +126,18 @@ int main() {
     if (rv) {
         die("connect");
     }
-    const char *query_list[3] = {"hello1", "hello2", "hello3"};
-    for (size_t i = 0; i < 3; ++i) {
-        int32_t err = send_req(fd, query_list[i]);
-        if (err) {
-            goto L_DONE;
-        }
+
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; ++i) {
+        cmd.push_back(argv[i]);
     }
-    for (size_t i = 0; i < 3; ++i) {
-        int32_t err = read_res(fd);
-        if (err) {
-            goto L_DONE;
-        }
+    int32_t err = send_req(fd, cmd);
+    if (err) {
+        goto L_DONE;
+    }
+    err = read_res(fd);
+    if (err) {
+        goto L_DONE;
     }
 
 L_DONE:
