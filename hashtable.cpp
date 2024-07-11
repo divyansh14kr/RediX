dddbd281bb2549e72429fc55fc4d9520f2bee692
Divyansh Kumar <divyansh14kr@gmail.com>
2024-07-11 12:32:00 +0530

    completed Hashtables

--- hashtable.cpp
+++ hashtable.cpp
@@ -42,4 +42,72 @@ static HNode *h_detach(HTab *htab, HNode **from){
     htab->size--;
     return node;
 }
+const size_t k_resizing_work = 128;
+
+static void hm_help_resizing(HMap *hmap){
+    size_t nwork = 0;
+    while(nwork<k_resizing_work && hmap->ht2.size >0 ){
+        HNode **from = &hmap->ht2.tab[hmap->resizing_pos];
+        if(!*from){
+            hmap->resizing_pos++;
+            continue;
+        }
+        h_insert(&hmap->ht1, h_detach(&hmap->ht2,from));
+        nwork++;
+    }
+    if(hmap->ht2.size == 0 && hmap->ht2.tab){
+        free(hmap->ht2.tab);
+        hmap->ht2 = HTab{};
+    }
+}
+static void hm_start_resizing(HMap *hmap){
+    assert(hmap->ht2.tab == NULL);
+    hmap->ht2 = hmap->ht1;
+    h_init(&hmap->ht1,(hmap->ht1.mask +1)*2);
+    hmap->resizing_pos =0;
+}
+HNode *hm_lookup(HMap *hmap, HNode *key, bool(*eq)(HNode *, HNode *)){
+    hm_help_resizing(hmap);
+    HNode **from = h_lookup(&hmap->ht1, key,eq);
+    from = from ? from: h_lookup(&hmap->ht2,key,eq);
+    return from ? *from : NULL;
+}
+const size_t k_max_load_factor = 8;
+
+void hm_insert(HMap *hmap, HNode *node){
+    if(!hmap->ht1.tab){
+        h_init(&hmap->ht1,4);
+    }
+    h_insert(&hmap->ht1,node);
+
+    if(!hmap->ht2.tab){
+        size_t load_factor = hmap->ht1.size / (hmap->ht1.mask +1);
+        if(load_factor >= k_max_load_factor){
+            hm_start_resizing(hmap);
+        }
+    }
+    hm_help_resizing(hmap);
+}
+HNode *hm_pop(HMap *hmap, HNode *key, bool(*eq)(HNode *, HNode *)){
+    hm_help_resizing(hmap);
+    if (HNode **from = h_lookup(&hmap->ht1, key, eq)) {
+        return h_detach(&hmap->ht1,from);
+    }
+    if(HNode **from = h_lookup(&hmap->ht2,key,eq)){
+        return h_detach(&hmap->ht2,from);
+    }
+    return NULL;
+}
+
+size_t hm_size(HMap *hmap){
+    return hmap->ht1.size + hmap->ht2.size;
+}
+void hm_destory(HMap *hmap){
+    free(hmap->ht1.tab);
+    free(hmap->ht2.tab);
+    *hmap = HMap{};
+}
+
+
+
 
--- hashtable.h
+++ hashtable.h
@@ -16,8 +16,13 @@ struct HTab{
 struct HMap{
     HTab ht1;
     HTab ht2;
-    size_t 
-}
+    size_t resizing_pos =0;
+};
+HNode *hm_lookup(HMap *hmap,HNode * key, bool(*eq)(HNode *, HNode * ));
+void hm_insert(HMap * hmap, HNode *node);
+HNode *hm_pop(HMap *hmap, HNode *key,bool (*eq)(HNode *, HNode*));
+size_t hm_size(HMap *hmap);
+void hm_destroy(HMap *hmap);
 
 
 
